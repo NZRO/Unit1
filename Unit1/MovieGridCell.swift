2946240d72a50aaf2e077da36d2bfb0b31d64616
Edison Lockwood <risingdowntothetop@hotmail.com>
2020-02-06 19:51:18 -0800

    Added Superhero grid

---
+++ Unit1/MovieGridCell.swift
@@ -0,0 +1,16 @@
+//
+//  MovieGridCell.swift
+//  Unit1
+//
+//  Created by EDISON LOCKWOOD on 2/6/20.
+//  Copyright © 2020 EDISON LOCKWOOD. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+
+}
